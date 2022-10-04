0e83a8386914f2a7ff794908002cf3473c569ea4
Devakumar D <devakumar.dhanapoosanam@syncfusion.com>
2022-10-04 12:35:53 +0530

    Added segment color customize sample.

---
+++ WinUI_Chart_Segment_Color/Model/Model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinUI_Chart_Segment_Color
+{
+    public class Model
+    {
+        public string XValue { get; set; }
+
+        public double YValue { get; set; }
+
+        public Model(string name, double count)
+        {
+            XValue = name;
+            YValue = count;
+        }
+    }
+}
